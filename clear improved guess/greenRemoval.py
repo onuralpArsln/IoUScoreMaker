fe04ac993d4f6024fd23d844004c2587807b7c83
hackerFaruk <onuralparslan97@gmail.com>
2024-05-08 00:34:30 +0300

    diffrent image methods appleid

--- clear improved guess/greenRemoval.py
+++ clear improved guess/greenRemoval.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from skimage.color import rgb2gray
 
 # Load the image
 img = cv2.imread('test.png')
@@ -11,15 +12,24 @@ b, g, r = cv2.split(img)
 new_img = cv2.merge((b, np.zeros_like(g), r))
 
 
-image_gray = cv2.rgb2gray(new_img)
-blur = cv2.GaussianBlur(image_gray,(3,3),0)
+
 
 # Display or save the result
 cv2.imshow('Image without green channel', new_img)
 cv2.imwrite('./channelClear.png', new_img) 
 
+
+image_gray = rgb2gray(new_img)
+blur = cv2.GaussianBlur(image_gray,(3,3),0)
+
 cv2.imshow('Image w/o green and lp', blur)
 cv2.imwrite('./channelClearFiltered.png', blur) 
+
+
+ret,thresh1 = cv2.threshold(blur,0,150,cv2.THRESH_BINARY)
+
+cv2.imshow('Image tresholded', thresh1)
+cv2.imwrite('./threshold.png', thresh1) 
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
